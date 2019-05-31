a8e2737d6c49e0d85a63cfe0130f71b1b75399d6
sho-machida <hogeoge@gmail.com>
2019-05-31 18:35:56 +0900

    データテーブルコンポーネントに登録フォーム機能をマージ。

--- src/components/ReferenceDataTable.vue
+++ src/components/ReferenceDataTable.vue
@@ -2,9 +2,8 @@
   <v-flex xs12>
     <h1>Data Table</h1>
     <div>
-      <!-- I haven't made any change to this template. This is only copy and paste from doc site. -->
       <v-dialog v-model="dialog">
-        <v-btn color="primary" dark slot="activator" class="mb-2">New Item</v-btn>
+        <v-btn color="primary" slot="activator" class="mb-4 mt-4">New Item</v-btn>
         <v-card>
           <v-card-title>
             <span class="headline">{{ formTitle }}</span>
@@ -12,49 +11,46 @@
           <v-card-text>
             <v-container grid-list-md>
               <v-layout wrap>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Dessert name" v-model="editedItem.name"></v-text-field>
+                <v-flex xs12>
+                  <v-text-field
+                    label="English"
+                    outline
+                    v-model="editedItem.english"
+                    @keyup="wordsCounter"
+                  ></v-text-field>
+                  <p>current word counter: {{currentWordsCount}}</p>
                 </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Calories" v-model="editedItem.calories"></v-text-field>
+                <v-flex xs12>
+                  <v-text-field label="translation" outline v-model="editedItem.translation"></v-text-field>
                 </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Fat (g)" v-model="editedItem.fat"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Carbs (g)" v-model="editedItem.carbs"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="Protein (g)" v-model="editedItem.protein"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="English" v-model="editedItem.english"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field label="translation" v-model="editedItem.translation"></v-text-field>
+                <v-flex xs12>
+                  {{ selectedPlan }}
+                  <v-select
+                    v-model="selectedPlan"
+                    item-text="label"
+                    item-value="value"
+                    :items="plans"
+                    label="旅行プラン"
+                    multiple
+                  />
                 </v-flex>
               </v-layout>
             </v-container>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+            <v-btn color="gray" @click.native="close">Cancel</v-btn>
+            <v-btn color="success" @click.native="save">Save</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
       <v-data-table :headers="headers" :items="items" hide-actions class="elevation-1">
         <template slot="items" slot-scope="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-right">{{ props.item.calories }}</td>
-          <td class="text-xs-right">{{ props.item.fat }}</td>
-          <td class="text-xs-right">{{ props.item.carbs }}</td>
-          <td class="text-xs-right">{{ props.item.protein }}</td>
-          <td class="text-xs-right">{{ props.item.english }}</td>
-          <td class="text-xs-right">{{ props.item.translation }}</td>
-          <td class="justify-center layout px-0">
+          <td>{{ props.item.english }}</td>
+          <td>{{ props.item.translation }}</td>
+          <td class="layout px-0">
             <v-btn icon class="mx-0" @click="editItem(props.item)">
-              <v-icon color="teal">edit</v-icon>
+              <v-icon color="blue">edit</v-icon>
             </v-btn>
             <v-btn icon class="mx-0" @click="deleteItem(props.item)">
               <v-icon color="pink">delete</v-icon>
@@ -70,20 +66,13 @@
 </template>
 
     <script>
+/*
+https://vuetifyjs.com/ja/components/selects#examples
+    */
 export default {
   data: () => ({
     dialog: false,
     headers: [
-      {
-        text: "Dessert (100g serving)",
-        align: "left",
-        sortable: false,
-        value: "name"
-      },
-      { text: "Calories", value: "calories" },
-      { text: "Fat (g)", value: "fat" },
-      { text: "Carbs (g)", value: "carbs" },
-      { text: "Protein (g)", value: "protein" },
       { text: "English", value: "english" },
       { text: "Translation", value: "translation" },
       { text: "Actions", value: "name", sortable: false }
@@ -91,23 +80,20 @@ export default {
     items: [],
     editedIndex: -1,
     editedItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
       english: "",
       translation: ""
     },
     defaultItem: {
-      name: "",
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
       english: "",
       translation: ""
-    }
+    },
+    currentWordsCount: 0,
+    selectedPlan: { label: "ドイツ", value: "germany" },
+    plans: [
+      { label: "ドイツ", value: "germany" },
+      { label: "スペイン", value: "spain" },
+      { label: "フランス", value: "france" }
+    ]
   }),
 
   computed: {
@@ -130,20 +116,10 @@ export default {
     initialize() {
       this.items = [
         {
-          name: "Frozen Yogurt",
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
           english: "aaaa bbbb",
           translation: "テスト1"
         },
         {
-          name: "Ice cream sandwich",
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
           english: "aaaa bbbb",
           translation: "テスト2"
         }
@@ -177,6 +153,11 @@ export default {
         this.items.push(this.editedItem);
       }
       this.close();
+    },
+
+    wordsCounter() {
+      var wordsNum = this.english.split(" ").length;
+      this.currentWordsCount = wordsNum;
     }
   }
 };
